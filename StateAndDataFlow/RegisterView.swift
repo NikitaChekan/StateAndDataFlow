2e28048f0e8453dada4871512a3e6d91080910f1
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2022-12-16 11:10:53 +0300

    add character count

--- StateAndDataFlow/RegisterView.swift
+++ StateAndDataFlow/RegisterView.swift
@@ -9,23 +9,33 @@ import SwiftUI
 
 struct RegisterView: View {
     @State private var name = ""
+    @State private var counter = 0
     @EnvironmentObject private var userManager: UserManager
     
     var body: some View {
         VStack {
-            TextField("Enter your name...", text: $name)
-                .multilineTextAlignment(.center)
+            HStack {
+                TextField("Enter your name...", text: $name)
+                    .multilineTextAlignment(.center)
+                    .onChange(of: name.count) { value in
+                        counter = value
+                    }
+                Text(counter.formatted())
+                    .foregroundColor(.red)
+            }
             Button(action: registerUser) {
                 HStack {
                     Image(systemName: "checkmark.circle")
                     Text("ОК")
                 }
+                .padding()
             }
         }
+        .padding()
     }
     
     private func registerUser() {
-        if !name.isEmpty {
+        if !name.isEmpty, name.count > 3 {
             userManager.name = name
             userManager.isRegister.toggle()
         }
